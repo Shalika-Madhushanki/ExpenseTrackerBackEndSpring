8c11b964e5668f01ada79fa6b04222258b5ee7d1
Shalika-Madhushanki <shali.madhushanki@gmail.com>
2024-11-02 17:05:37 +0100

    update frontend url

--- src/main/java/com/example/expensetrackerspring/config/WebConfig.java
+++ src/main/java/com/example/expensetrackerspring/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://localhost:5173", "https://shalika-madhushanki.github.io")
+                        .allowedOrigins("https://localhost:5173", "https://shalika-madhushanki.github.io/expense-tracker-frontend")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
